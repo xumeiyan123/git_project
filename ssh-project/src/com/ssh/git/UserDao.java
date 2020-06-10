a93eceb7c5e6ea8e5c00f34f7d336eb9583eae6c
xumeiyan123 <2530145839@qq.com>
2020-06-10 11:14:57 +0800

    对用户模块进行DDL语句操作

---
+++ ssh-project/src/com/ssh/git/UserDao.java
@@ -0,0 +1,28 @@
+package com.ssh.git;
+
+import java.util.List;
+
+public class UserDao {
+
+	public void createrUser(int id,String username,String password) {		
+		//添加操作
+		
+	}
+	
+	public void deleteUser(int id) {
+	
+	}
+	//删除操作
+	
+    public void updaterUser(int id,String newusername,String newpassword) {
+	
+	//更新操作
+	
+   }
+    
+    public List<Object> scanUser(int pageIndex,int pageSize){
+    	
+    	return null;
+    	
+    }
+}
